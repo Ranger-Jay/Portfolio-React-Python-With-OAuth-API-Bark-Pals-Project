fd008fea44892acf2402b0311a5bca81fced5039
Ranger-Jay <jayriet123@gmail.com>
2023-05-11 23:42:08 +0000

    fix home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-// import rigoImageUrl from "../../img/bark-pals-bg.png";
-import BgImageUrl from "../../img/bark-pals-bg.png";
+// import rigoImageUrl from "../../img/bark-pals.jpg";
+import BgImageUrl from "../../img/bark-pals.jpg";
 // import "../../img/bark-pal-bg.png";
 import "../../styles/home.css";
 
@@ -10,12 +10,12 @@ export const Home = () => {
 
   return (
     <div className="text-center mt-5">
-      <h1>Bark Pals</h1>
+      {/* <h1>Bark Pals</h1> */}
       <p>
         <img src={BgImageUrl} />
       </p>
 
-      <h4>A date for you and your best friend</h4>
+      <h4>A playdate for your best friend</h4>
     </div>
   );
 };
